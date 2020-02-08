9726e886040b5a6706d99cb11dc76ba1517d568f
Laura <practicante1@giro.com.mx>
2020-02-07 18:34:30 -0600

    Initial commit

---
+++ app/src/main/java/ertzil/com/huella/HomeActivity.java
@@ -0,0 +1,25 @@
+package ertzil.com.huella;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
+
+
